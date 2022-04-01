1ce9bbaa821ac1dbd2cd8ff5ea8a3758bcb77790
Tatu Nordström <Tatu.Nordstrom@metropolia.fi>
2022-04-01 11:14:44 +0300

    refactoring

--- client/src/App.js
+++ client/src/App.js
@@ -123,6 +123,10 @@ const App = () => {
     </Togglable>
   )
 
+  const sortBlogs = blogs => {
+    return blogs.sort((a, b) => b.likes - a.likes)
+  }
+
   return (
     <div>
       <h2>blogs</h2>
@@ -133,8 +137,7 @@ const App = () => {
           <p>{user.name} logged in <button onClick={handleLogout}>logout</button></p>
           {blogForm()}
           <div id="blogdiv">
-            {blogs.sort((a, b) => b.likes - a.likes),
-            blogs.map(blog =>
+            { sortBlogs(blogs).map(blog =>
               <Blog key={blog.id} blog={blog} deleteBlog={deleteBlog} likeBlog={likeBlog}/>
             )}
           </div>
